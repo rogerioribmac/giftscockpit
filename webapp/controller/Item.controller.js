552210612397693033f18425a26ff2ac5263fbf0
ROGERIO RIBEIRO <rogerio.ribeiromacedo@ext.europarl.europa.eu>
2025-08-13 17:15:45 +0000

    read file

--- webapp/controller/Item.controller.js
+++ webapp/controller/Item.controller.js
@@ -1,20 +1,64 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
 ],
-function (Controller) {
+function (Controller,MessageToast) {
     "use strict";
 
     return Controller.extend("com.ep.zgiftscockpit.controller.Item", {
         
         onInit: function () {
-            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
             oRouter.getRoute("RouteItem").attachPatternMatched(this._onRouteMatched, this);
         },
-          
+
+        onAttachmentSelection: function(oEvent){
+
+          oEvent.preventDefault();
+          const oItem = oEvent?.getParameter("item");
+          const sFileName = oItem?.getFileName();
+          const sMimeType = oItem?.getMediaType();
+          let sBase64 = oItem?.data("fileContent");
+
+          if (!sBase64) {
+              const oBundle = this.getView().getModel("i18n").getResourceBundle();
+              const sMessage = oBundle.getText("Item.AttachNoContent");
+              sap.m.MessageToast.show(sMessage);
+              return;
+          }
+
+          if (sBase64.startsWith("data:")) {
+              sBase64 = sBase64.split(",")[1];
+          }
+
+          const sByteCharacters = atob(sBase64);
+          const aByteNumbers = new Array(sByteCharacters.length);
+          for (let iIndex = 0; iIndex < sByteCharacters.length; iIndex++) {
+              aByteNumbers[iIndex] = sByteCharacters.charCodeAt(iIndex);
+          }
+
+          const aByteArray = new Uint8Array(aByteNumbers);
+          const oBlob = new Blob([aByteArray], { type: sMimeType });
+          const sUrl = URL.createObjectURL(oBlob);
+
+          const sMimeLower = sMimeType.toLowerCase();
+          if (sMimeLower.includes("excel") || sMimeLower.includes("word") || sMimeLower.includes("presentation")) {
+              const oLink = document.createElement("a");
+              oLink.href = sUrl;
+              oLink.download = sFileName;
+              document.body.appendChild(oLink);
+              oLink.click();
+              document.body.removeChild(oLink);
+          } else {
+              window.open(sUrl, "_blank");
+          }
+
+        },
+        
         _onRouteMatched: function (oEvent) {
 
-            var sReservationNo = oEvent.getParameter("arguments").reservationNo;
-            var oView = this.getView();
+            const sReservationNo = oEvent.getParameter("arguments").reservationNo;
+            const oView = this.getView();
 
             oView?.bindElement({
                 path: "/zz_pv_gifts_ckpt_reservations('" + sReservationNo + "')",
@@ -36,7 +80,7 @@ function (Controller) {
                       const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
                       oRouter.navTo("RouteNotFound");
                     } else {
-                      var oSmartTable = oView.byId("idSmartTableItems");
+                      const oSmartTable = oView.byId("idSmartTableItems");
                     }
                     
                   }.bind(this)
